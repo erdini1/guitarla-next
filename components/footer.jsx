fe2a46fc0b098d46630a08dab436e050a530ee7a
Dylan Erdini <erdini.dylan@gmail.com>
2023-05-28 12:51:12 -0300

    Creando el footer

--- components/footer.jsx
+++ components/footer.jsx
@@ -1,6 +1,29 @@
+import Link from "next/link"
+import styles from "@/styles/footer.module.css"
+
 const Footer = () => {
+
   return (
-    <div>Footer</div>
+    <footer>
+      <div className="contenedor">
+        <nav className={styles.navegacion}>
+          <Link
+            href="/"
+          >Inicio</Link>
+          <Link
+            href="/nosotros"
+          >Nosotros</Link>
+          <Link
+            href="/tienda"
+          >Tienda</Link>
+          <Link
+            href="/blog"
+          >Blog</Link>
+        </nav>
+        <p>Todos los derechos reservados {new Date().getFullYear()}</p>
+
+      </div>
+    </footer>
   )
 }
 

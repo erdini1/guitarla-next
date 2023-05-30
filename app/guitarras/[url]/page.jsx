cf71b02ab160edcf086c0a100e1d67d189399841
Dylan Erdini <erdini.dylan@gmail.com>
2023-05-30 12:49:09 -0300

    Agregando metadata y obteniendo datos de la guitarra

--- app/guitarras/[url]/page.jsx
+++ app/guitarras/[url]/page.jsx
@@ -1,7 +1,24 @@
 
 
-const Producto = ({ params }) => {
+export async function generateMetadata({ params, searchParams }) {
+    const { data } = await getGuitarra(params.url)
+    const { nombre } = data[0].attributes
+    return {
+        title: `GuitarLA - Guitarra ${nombre}`,
+        description: `GuitarLA - Guitarra ${nombre}, Venta de guitarras, blog de musica`
+    }
+}
+
+async function getGuitarra(url) {
+    const res = await fetch(`${process.env.API_URL}/guitarras/?filters[url]=${url}&populate=imagen`)
+    return res.json()
+}
+
+const Producto = async ({ params }) => {
     const { url } = params
+    const { data: guitarra } = await getGuitarra(url)
+    console.log(guitarra)
+
     return (
         <div>{url}</div>
     )
